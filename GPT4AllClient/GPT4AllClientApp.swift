8a089325bd3ec683a86024288a4088bf8c85ea06
Wontai Ki <ds1adr@gmail.com>
2025-10-29 23:58:07 -0700

    Initial Project

---
+++ GPT4AllClient/GPT4AllClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPT4AllClientApp.swift
+//  GPT4AllClient
+//
+//  Created by Wontai Ki on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct GPT4AllClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
